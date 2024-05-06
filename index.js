bd1196b3f548d733a92f3ac35457a96f3b502d7d
Razzoy <1081892@ucn.dk>
2024-05-06 13:41:35 +0200

    update v4

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import http from 'http';
 
+const port = 5040;
+
 http.createServer((request, response) => {
-    console.log('Server is running on http://localhost:4000');
-}).listen(4000)
+    console.log(`Server is running on http://localhost:${port}`);
+    response.writeHead(200,{ 'Content-Type': 'text/plain' });
+    response.write('Hello World');
+    response.end();
+}).listen(port)
